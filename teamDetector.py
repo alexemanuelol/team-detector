04707e406235b6a8fe5f9a84f989647cb6cbad6b
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-07-27 21:52:55 +0200

    Add network visualization

--- teamDetector.py
+++ teamDetector.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python3
 
+description =\
+"""
+Team detection program for the game Rust. The program goes through the
+friendlist of the player you provided and compares the friendlist with the
+battlemetrics page for the Rust server you provided. If matches are found, the
+program continues to go through their friendslists and compare with the
+battlemetrics page players, ultimately creating a network of players that are
+potentially in a team.
+"""
+
 import re
 import argparse
 import requests
-
+import networkx as nx
+from pyvis.network import Network
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-b', type=str, required=True, help='Battlemetrics Page for the Rust Server.')
-    parser.add_argument('-f', type=str, required=True, help='Friendlist of the person you want to inspect.')
+    parser.add_argument('-s', type=str, required=True, help='Steam page of the person you want to inspect.')
     args = parser.parse_args()
+
+    G = nx.Graph()
 
     battlemetricsPlayers = get_battlemetrics_players(args.b)
 
-    initialFriendlist = get_friendlist(args.f)
+    initialFriendlist = get_friendlist(args.s)
     friends = { initialFriendlist['steamId']: initialFriendlist['name']}
     leftToCheck = compare_players(battlemetricsPlayers, initialFriendlist['friends'])
+    for friend in leftToCheck:
+        G.add_edges_from([(initialFriendlist['name'], friend[1])])
 
     while True:
         if len(leftToCheck) == 0:
@@ -26,13 +41,22 @@ def main():
             friendlist = get_friendlist(f'https://steamcommunity.com/profiles/{steamId}/friends')
             friends[friendlist['steamId']] = friendlist['name']
             for steamIdC, nameC in compare_players(battlemetricsPlayers, friendlist['friends']):
+                G.add_edges_from([(friendlist['name'], nameC)])
                 if steamIdC not in friends and not any(steamIdC in x for x in newLeft):
                     newLeft.append([steamIdC, nameC])
 
         leftToCheck = newLeft
 
+    nt = Network('2000px', '2000px')
+    nt.from_nx(G)
+    nt.repulsion(damping=1)
+    nt.show('team_network.html')
+
+    print('Team Detector Result:\n')
+    print('Name:'.ljust(34) + 'SteamID:'.ljust(19) + 'Link:')
+
     for steamId, name in friends.items():
-        print(f'{name}: {steamId}')
+        print(f'{name}'.ljust(34) + f'{steamId}'.ljust(19) + f'https://steamcommunity.com/profiles/{steamId}')
 
 def scrape(url):
     try:
@@ -64,11 +88,10 @@ def get_friendlist(url):
     if content == False:
         print('Could not scrape friendlist page')
         exit()
-
-    regex = r'"steamid":"(.+?)","personaname":"(.+?)"'
-    info = re.findall(regex, content)[0]
-    steamId = info[0]
-    name = info[1]
+    regex = r'<meta property="og:title" content="(.+?)">'
+    name = re.findall(regex, content)[0]
+    regex = r',"steamid":"(.+?)",'
+    steamId = re.findall(regex, content)[0]
     regex = r'data-steamid="(.+?)".*?<div class="friend_block_content">(.+?)<br>'
     friends = re.findall(regex, content, re.MULTILINE|re.S)
 
